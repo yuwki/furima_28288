4891b64f18cfdd9587ecb74b660a0ccd7849a2fa
yuwki <yuuki.ishii1208@gmail.com>
2020-08-06 10:50:46 +0900

    devise導入・usersテーブル作成

--- db/migrate/20200805101822_devise_create_users.rb
+++ db/migrate/20200805101822_devise_create_users.rb
@@ -1,20 +1,21 @@
-# frozen_string_literal: true
-
 class DeviseCreateUsers < ActiveRecord::Migration[6.0]
   def change
     create_table :users do |t|
       ## Database authenticatable
-      t.string :name,               null: false
-      t.string :email,              null: false, default: ""
+      t.string :nickname,           null: false
+      t.string :email,              null: false, default: "", unipue: true
       t.string :encrypted_password, null: false, default: ""
-
+      t.string :first_name,         null: false
+      t.string :last_name,          null: false
+      t.string :first_name_kana,    null: false
+      t.string :last_name_kana,     null: false
+      t.date   :birth_date,         null: false
       ## Recoverable
       t.string   :reset_password_token
       t.datetime :reset_password_sent_at
 
       ## Rememberable
       t.datetime :remember_created_at
-
       ## Trackable
       # t.integer  :sign_in_count, default: 0, null: false
       # t.datetime :current_sign_in_at
