c2fe81a271cb96313c74bac311fc1a885a05ab45
yuwki <yuuki.ishii1208@gmail.com>
2020-08-07 11:13:35 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,22 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable
 
+  has_many :items
+  #has_many :comments
+  #has_many :orders
+  
+  validates :nickname, presence: true
+  validates :email, presence: true, uniqueness: true, format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i}
+  validates :password, presence: true, length: { minimum: 6, message: '6文字以上入力してください' }
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ン一-龥]+\z/, message: '全角文字を使用してください' } do
+    validates :first_name
+    validates :last_name
+    validates :first_name_kana
+    validates :last_name_kana
+    validates :first_name
+  end
+  
+   validates :birth_date, presence: true
 end
