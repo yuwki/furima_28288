986c607396d5b72c7c22525a580f68f6a163d722
yuwki <yuuki.ishii1208@gmail.com>
2020-08-17 19:57:00 +0900

    正規表現の変数化の修正

--- app/forms/item_order.rb
+++ app/forms/item_order.rb
@@ -3,12 +3,16 @@ class ItemOrder
   include ActiveModel::Model
   attr_accessor :post_number, :prefecture, :city, :house_number, :building_name, :phone_number, :token, :item_id, :user_id, :order_id
 
+  POSTAL_CODE_REGEX = /\A\d{3}[-]\d{4}\z/
+  CITY_CODE_REGEX = /\A[ぁ-んァ-ン一-龥]/
+  TEL_CODE_REGEX = /\A[a-zA-Z0-9]+\z/
+
   with_options presence: true do
-    validates :post_number, format: {with: /\A\d{3}[-]\d{4}\z/, message: "is invalid. Include hyphen(-)"}
+    validates :post_number, format: {with: POSTAL_CODE_REGEX, message: "is invalid. Include hyphen(-)"}
     validates :prefecture,  numericality: { other_than: 1, message: "is invalid. Input select prefectures." }
-    validates :city, format: {with:/\A[ぁ-んァ-ン一-龥]/, message: "is invalid. Input your city infomations."}
+    validates :city, format: {with: CITY_CODE_REGEX, message: "is invalid. Input your city infomations."}
     validates :house_number
-    validates :phone_number, format: {with: /\A[a-zA-Z0-9]+\z/, message: "is invalid. Remove hyphen(-).(maxlength 11)"}
+    validates :phone_number, format: {with: TEL_CODE_REGEX, message: "is invalid. Remove hyphen(-).(maxlength 11)"}
     validates :token
   end
 
