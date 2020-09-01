9b96b2d57b95b4a372ebc67d621117608fa600c3
yuwki <yuuki.ishii1208@gmail.com>
2020-09-01 12:56:40 +0900

    deproy.rb編集

--- config/deploy.rb
+++ config/deploy.rb
@@ -5,7 +5,7 @@ lock '3.14.1'
 set :application, 'furima_28288'
 
 # どのリポジトリからアプリをpullするかを指定する
-set :repo_url,  'git@github.com:yuwki/furima_28288.git
+set :repo_url,  'git@github.com:yuwki/furima_28288.git'
 
 # バージョンが変わっても共通で参照するディレクトリを指定
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
